4ca1120b673f882537297b3c1f9382448c5317fc
DaviQzR <125469425+DaviQzR@users.noreply.github.com>
2023-10-04 14:12:26 -0300

    Final commit Gestão de Funcionários

---
+++ src/Model/Funcionario.java
@@ -0,0 +1,108 @@
+package Model;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.text.ParseException;
+
+public class Funcionario {
+
+    private long id;
+    private String nome;
+    private String matricula;
+    private LocalDate admissao;
+    private LocalDate demissao;
+    private float salario;
+    private LocalTime horario;
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    DateTimeFormatter hora = DateTimeFormatter.ofPattern("HH:mm:ss");
+    DecimalFormat formatoSalario = new DecimalFormat("#,##0.00");
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getAdmissao() {
+        return admissao;
+    }
+
+    public void setAdmissao(LocalDate admissao) {
+        this.admissao = admissao;
+    }
+
+    public void setAdmissao(String data) {
+        LocalDate admissao = LocalDate.parse(data, dtf);
+        setAdmissao(admissao);
+    }
+
+    public LocalDate getDemissao() {
+        return demissao;
+    }
+
+    public void setDemissao(String data) {
+        demissao = LocalDate.parse(data, dtf);
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public String getSalarioFormatado() {
+        return formatoSalario.format(salario);
+    }
+
+    public void setSalarioFormatado(String salarioFormatado) throws ParseException {
+        salario = formatoSalario.parse(salarioFormatado).floatValue();
+    }
+
+    public LocalTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalTime horario)  {
+        this.horario = horario;
+    }
+
+    public void setHorario(String horario) throws DateTimeParseException   {
+        this.horario = LocalTime.parse(horario, hora);
+    }
+
+    public void exibir() {
+        System.out.println("Dados do Funcionário:");
+        System.out.println("ID: " + getId());
+        System.out.println("Nome: " + getNome());
+        System.out.println("Matrícula: " + getMatricula());
+        System.out.println("Admissão: " + getAdmissao().format(dtf));
+        System.out.println("Demissão: " + (getDemissao() != null ? getDemissao().format(dtf) : "Não especificada"));
+        System.out.println("Salário: " + getSalarioFormatado());
+        System.out.println("Horário: " + getHorario().format(hora));
+    }
+
+}
---
+++ src/View/GestaoFuncionario.java
@@ -0,0 +1,172 @@
+package View;
+
+import java.text.ParseException;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+import Model.Funcionario;
+
+public class GestaoFuncionario {
+
+  private int indice = 0;
+  public static final int MAX_SIZE = 50;
+  private final Funcionario[] funcionarios = new Funcionario[MAX_SIZE];
+  private final Scanner input = new Scanner(System.in);
+
+  public void menu() {
+    while (true) {
+      System.out.println("Gestão de Funcionários");
+      System.out.println("(C)riar");
+      System.out.println("(E)xibir");
+      System.out.println("(R)emover");
+      System.out.println("(A)tualizar");
+      System.out.println("(S)air");
+      char opcao = input.nextLine().toUpperCase().charAt(0);
+      if (opcao == 'C') {
+        criar();
+      } else if (opcao == 'E') {
+        exibir();
+      } else if (opcao == 'R') {
+        remover();
+      } else if (opcao == 'A') {
+        atualizar();
+      } else if (opcao == 'S') {
+        System.out.println("Até Breve ");
+        break;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    new GestaoFuncionario().menu();
+  }
+
+  public void criar() {
+    System.out.println("Cadastro de novo Funcionário");
+    Funcionario func = new Funcionario();
+    if (indice < MAX_SIZE) {
+      System.out.println("Informe o nome do Funcionario");
+      func.setNome(input.nextLine());
+
+      System.out.println("Informe a matricula do Funcionario");
+      func.setMatricula(input.nextLine());
+
+      System.out.println("Informe a data de admissão");
+      String strDate = input.nextLine();
+      func.setAdmissao(strDate);
+
+      System.out.println("Informe a data de demissão do Funcionário");
+      String Date = input.nextLine();
+      func.setDemissao(Date);
+
+      System.out.println("Informe o salário do Funcionário");
+      String salario = input.nextLine();
+      try {
+        func.setSalarioFormatado(salario);
+      } catch (ParseException e) {
+        System.out.println("Erro ao converter o salário. Certifique-se de que a entrada seja um número válido.");
+      }
+
+      System.out.println("Informe a hora do Funcionário");
+      String hora = input.nextLine();
+      try {
+        func.setHorario(hora);
+      } catch (DateTimeParseException e) {
+        System.out
+            .println("Erro ao converter a hora. Certifique-se de que a entrada esteja no formato correto (HH:mm:ss).");
+      }
+      func.setId(indice);
+      funcionarios[indice] = func;
+      indice++;
+    }
+  }
+
+  public void exibir() {
+    System.out.println("Exibindo os dados do Funcionário");
+    System.out.println("Informe a matrícula:");
+    String matricula = input.nextLine();
+    int i = pesquisar(matricula);
+    if (i >= 0) {
+      Funcionario func = funcionarios[i];
+      System.out.println("Detalhes do Funcionário:");
+      System.out.println("Nome: " + func.getNome());
+      System.out.println("Matrícula: " + func.getMatricula());
+      System.out.println("Admissão: " + func.getAdmissao());
+      System.out.println("Demissão: " + func.getDemissao());
+      System.out.println("Salário: " + func.getSalario());
+      System.out.println("Hora: " + func.getHorario());
+      System.out.println("ID: " + func.getId());
+    } else {
+      System.out.println("Não foi possível encontrar o funcionário com a matrícula fornecida.");
+    }
+  }
+
+  public int pesquisar(String matricula) {
+    for (int i = 0; i < MAX_SIZE; i++) {
+      Funcionario func = funcionarios[i];
+      if (func != null && matricula.equals(func.getMatricula())) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public void remover() {
+    System.out.println("Exclusão do Funcionário");
+    System.out.println("Informe a matricula do Funcionário");
+    String matricula = input.nextLine();
+    int i = pesquisar(matricula);
+    if (i >= 0) {
+      funcionarios[i] = null;
+    }
+  }
+
+  public void atualizar() {
+    System.out.println("Atualização do Funcionário");
+    System.out.println("Informe a matrícula do Funcionário que deseja atualizar");
+    String matricula = input.nextLine();
+    int i = pesquisar(matricula);
+
+    if (i >= 0) {
+      Funcionario func = funcionarios[i];
+      System.out.println("Digite o novo nome do Funcionário");
+      func.setNome(input.nextLine());
+
+      System.out.println("Digite a nova matrícula do Funcionário");
+      func.setMatricula(input.nextLine());
+
+      System.out.println("Digite a nova data de admissão do Funcionário (dd/mm/yyyy)");
+      String strDateAdmissao = input.nextLine();
+      func.setAdmissao(strDateAdmissao);
+
+      System.out.println("Digite a nova data de demissão do Funcionário (dd/mm/yyyy)");
+      String strDateDemissao = input.nextLine();
+      func.setDemissao(strDateDemissao);
+
+      System.out.println("Digite o novo salário do Funcionário");
+      String salario = input.nextLine();
+      try {
+        func.setSalarioFormatado(salario);
+        System.out.println("Salário atualizado com sucesso: " + func.getSalarioFormatado());
+      } catch (ParseException e) {
+        System.out.println("Erro ao atualizar o salário. Certifique-se de inserir um valor válido.");
+      }
+
+      System.out.println("Digite a nova hora do Funcionário (HH:mm:ss)");
+      String hora = input.nextLine();
+      func.setHorario(hora);
+      System.out.println("Hora atualizada com sucesso: " + func.getHorario());
+    } else {
+      System.out.println("Funcionário não encontrado.");
+    }
+  }
+
+  public void listar() {
+    System.out.println("Listagem dos Funcionários");
+    for (int i = 0; i < MAX_SIZE; i++) {
+      Funcionario func = funcionarios[i];
+      if (func != null) {
+        System.out.println(func);
+      }
+    }
+  }
+}
